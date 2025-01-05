4bafa481ff4ec50a2c0e753653ca8a7e4a26df39
s3nsh1 <shubham228pandey@gmail.com>
2025-01-05 22:11:44 +0530

    update

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Dashboard: React.FC = () => {
-  return <div>Displaying a Dashboard</div>;
+  return (
+    <div>
+      <div className='min-h-screen flex justify-center items-center bg-blue-500'>
+        <h1 className='text-4xl text-white font-bold'>
+          Tailwind CSS is Working!
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;

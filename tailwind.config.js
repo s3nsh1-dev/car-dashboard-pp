f36e41cbc446b1e7ca931fa00e243608775f03c0
s3nsh1 <shubham228pandey@gmail.com>
2025-01-05 22:11:52 +0530

    create

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", // Adjust if `index.html` is in another folder
+    "./src/**/*.{js,ts,jsx,tsx}", // Includes all your component files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

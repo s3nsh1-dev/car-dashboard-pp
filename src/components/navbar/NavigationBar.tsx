6b4a15d79d50b3785b7f55ff72dbf53e4a7f403e
s3nsh1-dev <shubham228pandey@gmail.com>
2025-01-06 03:42:36 +0530

    Navbar in progress

--- src/components/navbar/NavigationBar.tsx
+++ src/components/navbar/NavigationBar.tsx
@@ -1,10 +1,39 @@
-import React from "react";
-import { AppBar, Toolbar } from "@mui/material";
+import { useState } from "react";
+import { AppBar, Toolbar, Box, Typography } from "@mui/material";
+import MenuOpenIcon from "@mui/icons-material/MenuOpen";
+import CloseFullscreenIcon from "@mui/icons-material/CloseFullscreen";
+import IconButton from "@mui/material/IconButton";
+import TraverseBox from "./TraverseBox";
 
 const NavigationBar = () => {
+  const [iconShow, setIconShow] = useState(false);
+  const toggleIcon = () => {
+    setIconShow(!iconShow);
+  };
   return (
-    <AppBar>
-      <Toolbar></Toolbar>
+    <AppBar position='sticky'>
+      <Toolbar sx={{ justifyContent: "space-between" }}>
+        <Typography>Car Showroom</Typography>
+        <Box sx={{ display: { xs: "none", sm: "flex" } }}>
+          <TraverseBox />
+        </Box>
+        <Box sx={{ display: { xs: "flex", sm: "none" } }}>
+          <IconButton
+            edge='start'
+            onClick={toggleIcon}
+            aria-label='open drawer'
+            sx={{
+              display: { xs: "flex", sm: "none" },
+            }}
+          >
+            {iconShow ? (
+              <CloseFullscreenIcon sx={{ color: "white" }} />
+            ) : (
+              <MenuOpenIcon sx={{ color: "white" }} />
+            )}
+          </IconButton>
+        </Box>
+      </Toolbar>
     </AppBar>
   );
 };

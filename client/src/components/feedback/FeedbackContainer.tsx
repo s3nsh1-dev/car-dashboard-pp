02f980f417c698e0b84500fa8fc893a6943df270
s3nsh1 <shubham228pandey@gmail.com>
2025-02-23 23:47:23 +0530

    Feedback done

--- client/src/components/feedback/FeedbackContainer.tsx
+++ client/src/components/feedback/FeedbackContainer.tsx
@@ -1,12 +1,38 @@
-import React from "react";
-import { Typography } from "@mui/material";
+import { Typography, TextField, Button, Box } from "@mui/material";
 
 const FeedbackContainer = () => {
+  console.log("hello");
   return (
-    <>
-      <Typography variant='h5'>Please share your valuable feedback</Typography>
-      <textarea />
-    </>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        border: "1px solid #ccc",
+        borderRadius: "5px",
+        p: 2,
+        width: "50vw",
+        margin: "0 auto",
+        gap: 2,
+      }}
+    >
+      <Typography variant='h5' fontWeight='bold'>
+        Please share your valuable feedback
+      </Typography>
+      <TextField
+        id='outlined-multiline-static'
+        label='Your Feedback'
+        multiline
+        rows={6}
+        sx={{ width: "40vw" }}
+      />
+      <Box>
+        <Button variant='contained' color='success'>
+          Share
+        </Button>
+      </Box>
+    </Box>
   );
 };
 
